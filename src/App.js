c3516a989c853d3fd15768a6ee710cdac8d29bc9
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-07-19 03:36:48 +0900

    REACT_APP_PUBLIC_URL 설정

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <AppContainer>
         <Header cartItems={cartItems} />
         <Routes>
